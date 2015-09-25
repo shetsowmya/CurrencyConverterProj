babf4432f2e1f5fa00e1d93d8cbe037af3d0d361
sowmya <shetsowmya24@gmail.com>
2015-09-26 07:21:16 +1000

    Initial Commit

---
+++ CurrencyConverter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CurrencyConverter
+//
+//  Created by Nidhi on 26/09/15.
+//  Copyright © 2015 ShetCo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ CurrencyConverterTests/CurrencyConverterTests.m
@@ -0,0 +1,39 @@
+//
+//  CurrencyConverterTests.m
+//  CurrencyConverterTests
+//
+//  Created by Nidhi on 26/09/15.
+//  Copyright © 2015 ShetCo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CurrencyConverterTests : XCTestCase
+
+@end
+
+@implementation CurrencyConverterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
